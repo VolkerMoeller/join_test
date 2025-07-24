c0d8f435607cad78209f300d8393678cdf7ef4a9
VolkerMoeller <126388919+VolkerMoeller@users.noreply.github.com>
2025-07-24 15:49:34 +0200

    new idea great:)

--- js/pages/signPages/logIn.js
+++ js/pages/signPages/logIn.js
@@ -1,9 +1,36 @@
 async function initLogIn() {
     await includeHTML();
+    animBg();
     animLogo();
+    animClr();
+    hideOvl();
 }
 
-function animLogo() {
+
+function animBg() {
     const ovlAnim = document.querySelector('.ovl-anim');
-    ovlAnim.classList.add('fst-page-bg-anim');
+    ovlAnim.classList.add('bg-anim');
+}
+
+
+function animLogo() {
+    const logos = document.querySelectorAll('.logo-desktop-main');
+    logos.forEach(logo => {
+        logo.classList.add('logo-anim');
+    });
+}
+
+
+function animClr() {
+    const paths = document.querySelectorAll('.path-def-1st');
+    paths.forEach(path => {
+        path.classList.add('clr-anim');
+    });
+}
+
+
+function hideOvl() {
+    const ovlFrame = document.querySelector('.ovl-frame');
+    ovlFrame.classList.add('hide-dly');
+
 }

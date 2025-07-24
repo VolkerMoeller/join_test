0d1f9948b684df4b22e6e02c01ee3bf9687b5020
VolkerMoeller <126388919+VolkerMoeller@users.noreply.github.com>
2025-07-24 14:31:58 +0200

    new idea

--- js/pages/signPages/logIn.js
+++ js/pages/signPages/logIn.js
@@ -1,29 +1,9 @@
 async function initLogIn() {
     await includeHTML();
-    showBody();
-    // hideLogoSVG();
+    animLogo();
 }
 
-
-// To prevent the logo from “flickering”, I have previously displayed it as an svg file. Now it is no longer needed.
-
-function hideLogoSVG() {
-    const logoSVG = document.getElementById('logoSVG');
-    logoSVG.classList.add('display-none');
-}
-
-
-// show Body with delay
-
-function showBody() {
-    setTimeout(() => {
-        document.getElementById('logIn').classList.add('show');
-    }, 550);
-}
-
-
-// helpers
-
-function index() {
-    location.assign('./index.html');
+function animLogo() {
+    const ovlAnim = document.querySelector('.ovl-anim');
+    ovlAnim.classList.add('fst-page-bg-anim');
 }

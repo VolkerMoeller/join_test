5b60aed928dc89e9c48071978d901ae654e76f1d
VolkerMoeller <v.moeller@t-online.de>
2025-08-27 20:33:21 +0200

    27082025_2033: colorSchem 2nd

--- js/pages/infoPages/legal.js
+++ js/pages/infoPages/legal.js
@@ -1,4 +1,5 @@
 async function initLegal() {
     await includeHTML();
+    setClrSchemeInit();
     invertLogoClr();
 }
--- js/pages/infoPages/privacy.js
+++ js/pages/infoPages/privacy.js
@@ -1,4 +1,5 @@
 async function initPrivacy() {
     await includeHTML();
+    setClrSchemeInit();
     invertLogoClr();
 }

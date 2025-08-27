428400cef069ff910d845c935d23fd7b6b0e1e74
Volker Möller <v.moeller@t-online.de>
2025-08-27 16:12:18 +0200

    20230827_1512: input icon 2nd

--- js/pages/sign.js
+++ js/pages/sign.js
@@ -1,8 +1,27 @@
 async function initSign() {
     await includeHTML();
     focusInputBy('eMail');
+
 }
+
 
 function focusInputBy(id) {
     document.getElementById(id).focus();
+}
+
+
+function checkLogInInput() {
+    checkValidInput('eMail');
+    checkValidInput('password');
+}
+
+function checkValidInput(id) {
+    let inputField = document.getElementById(id);
+    if (inputField.checkValidity()) {
+        inputField.classList.remove('invalid-input');
+        return true;
+    } else {
+        inputField.classList.add('invalid-input');
+        return false;
+    }
 }

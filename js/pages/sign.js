40be7ae09bd902ee66bb9ef6a8d53e22cf16624b
Volker Möller <v.moeller@t-online.de>
2025-09-03 09:24:55 +0200

    20250903: bugg cnt-right button to top

--- js/pages/sign.js
+++ js/pages/sign.js
@@ -133,7 +133,7 @@ function warningTextOff() {
 
 
 function toggleSignElementsOnTop() {
-    let selectors = ['.input-container', '.form-sign input', '.content-right button', '.outside-bottom a', '.form-btn-frame', '.clr-scheme-panel'];
+    let selectors = ['.input-container', '.form-sign input', '.content-right button', '.outside-bottom a', 'cnt-right button', '.form-btn-frame', '.clr-scheme-panel'];
     for (let i = 0; i < selectors.length; i++) {
         let elements = document.querySelectorAll(selectors[i]);
         if (elements) {

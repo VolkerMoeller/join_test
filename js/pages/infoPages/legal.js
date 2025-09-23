d295ae2a89a4e85dc9b502532c9fc6cd90f0b85f
Volker Moeller <v.moeller@t-online.de>
2025-09-23 12:53:59 +0200

    20250923: nav mobile 8th

--- js/pages/infoPages/legal.js
+++ js/pages/infoPages/legal.js
@@ -10,6 +10,7 @@ async function initLegal() {
     invertLogoClr();
     defaultHeaderView();
     navigationView();
+    setNavBtnMblSelected();
 }
 
 
@@ -22,3 +23,11 @@ async function initLegal() {
 // invertLogoClr() --> main.js
 // defaultHeaderView() --> main.js
 // navigationView() --> info.js
+// setNavBtnMblSelected()
+
+function setNavBtnMblSelected() {
+    let navBtnMbl = document.getElementById('txtBtnLegalMbl');
+    navBtnMbl.disabled = true;
+    navBtnMbl.classList.remove('txt-btn-btm');
+    navBtnMbl.classList.add('txt-btn-btm-selected');
+}

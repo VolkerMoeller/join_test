66f9089f069731bad210c73794b909124b09abe6
Volker Moeller <v.moeller@t-online.de>
2025-09-29 16:02:05 +0200

    fade out overlay 3rd

---
+++ js/generateHTML/genHTMLElements.js
@@ -0,0 +1,24 @@
+function genHTMLHvrBtn(btnId, onclick) {
+    return `
+    <button id="${btnId}Hvr" class="display-none" onclick="${onclick}"
+        onmouseleave="toggleElements('${btnId}','${btnId}Hvr')" w3-include-hvr-btn="./assets/img/svgHTML/6-leftArrowHover.html">
+    </button>
+    <button id="${btnId}" onmouseover="toggleElements('${btnId}','${btnId}Hvr')"
+        w3-include-hvr-btn="./assets/img/svgHTML/7-leftArrow.html">
+    </button>
+  `;
+}
+
+function genHTMLSmallMenu() {
+    return `
+    <div class="ovl" onclick="hideSmallMenu()">
+        <div class="nav-cnt nav-top-rgt display-none">
+            <button id="helpBtnSmallMenu" class="version-2-body"
+                onclick="showInfoContentById('cntCenterHelp')">Help</button>
+            <button class="version-2-body" onclick="showInfoContentById('cntCenterLegal')">Legal Notice</button>
+            <button class="version-2-body" onclick="showInfoContentById('cntCenterPrivacy')">Privacy Policy</button>
+            <button class="version-2-body" onclick="switchToSignPages()">Log out</button>
+        </div>
+    </div>
+    `;
+}

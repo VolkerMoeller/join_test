5cd22ebaff159d5460e7192e45fd851f1e1f7976
Volker Moeller <v.moeller@t-online.de>
2025-10-02 13:09:36 +0200

    assignde add task 12th

--- js/pages/workPages/add.js
+++ js/pages/workPages/add.js
@@ -6,4 +6,11 @@
 
 function downDropOpen() {
     console.log('downDropOpen()');
+    document.getElementById('dropDownOpen').classList.remove('display-none');
+    document.getElementById('dropDownClose').classList.add('display-none');
+    document.getElementById('arrDropOpn').classList.remove('display-none');
+    document.getElementById('arrDropOpnHvr').classList.add('display-none');
+    document.getElementById('arrDropClo').classList.remove('display-none');
+    document.getElementById('arrDropCloHvr').classList.add('display-none');
+    document.getElementById('assigned').value = "";
 }

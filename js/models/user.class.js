b89710e7d96b2456b1ffd3e9f30304c10f6a8461
Volker Moeller <v.moeller@t-online.de>
2025-10-10 10:14:11 +0200

    20251010: firebase 4th

---
+++ js/models/user.class.js
@@ -0,0 +1,32 @@
+class User {
+    id = '';
+    name = '';
+    eMail = '';
+    password = '';
+    initial = '';
+    color = '';
+
+    constructor(userData) {
+        this.id = 'userId' + userData[0];
+        this.name = userData[1];
+        this.eMail = userData[2];
+        this.password = userData[3];
+        this.getInitials();
+        this.getUserColor();
+    }
+
+    getInitials() {
+        let splittedName = this.name.split(" ");
+        for (let i = 0; i < splittedName.length; i++) {
+            let element = splittedName[i].charAt(0);
+            this.initial = this.initial + element;
+            this.initial = this.initial.toUpperCase();
+            this.initial = this.initial.substring(0, 2);
+        }
+    }
+
+    getUserColor() {
+        let colorNr = Math.floor(Math.random() * 15);
+        this.color = `var(--variant${colorNr})`;
+    }
+}
--- js/pages/sign.js
+++ js/pages/sign.js
@@ -115,6 +115,8 @@ function initCheckInputSignUp() {
     }
     if (success) {
         initanimSignMessages();
+        // XXX
+        doSth('firebase');
     }
 }
 

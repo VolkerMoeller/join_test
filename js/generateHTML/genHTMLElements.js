b294af5a8211c837472d30f1637b3e52df3f200b
Volker Möller <v.moeller@t-online.de>
2025-12-10 13:26:45 +0100

    clean code

--- js/generateHTML/genHTMLElements.js
+++ js/generateHTML/genHTMLElements.js
@@ -1,15 +1,3 @@
-// function genHTMLHvrBtn(btnId, onclick) {
-//     return `
-//     <button id="${btnId}Hvr" class="display-none" onclick="${onclick}"
-//         onmouseleave="toggleElements('${btnId}','${btnId}Hvr')" w3-include-hvr-btn="./assets/img/svgHTML/6-leftArrowHover.html">
-//     </button>
-//     <button id="${btnId}" onmouseover="toggleElements('${btnId}','${btnId}Hvr')"
-//         w3-include-hvr-btn="./assets/img/svgHTML/7-leftArrow.html">
-//     </button>
-//   `;
-// }
-
-
 function genHTMLHvrBtn(iconId) {
     return `
         <button id="${iconId}Hvr"
